0b9478ac04980cbacb43d29fe268bdaf2e1d54b1
Minseok Kim <mk7545@nyu.edu>
2024-09-13 23:05:02 -0400

    Initialization

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape(url):
+    # TODO
+    print(url)
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Failed to scrape. Code: {response.status_code}")
+            return -1
+        # Problem here
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+    except Exception as e:
+        print(f"Error: {e}")
+
+def main():
+    url = ""
+    result = scrape(url)
+    print(result)
+
+if __name__ == "__main__":
+    main()
